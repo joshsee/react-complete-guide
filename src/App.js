7dfa6203891933deb61a55b48ab9cdb0e26496b0
jsee-twilio <ychian@gmail.com>
2020-10-27 19:56:00 +0800

    Clean up some files

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1></h1>
+        <h1>Hi my react app</h1>
       </div>
     );
   }
