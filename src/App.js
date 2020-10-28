69d40ae868b93a30055f916984071108cc2c2cdf
jsee-twilio <ychian@gmail.com>
2020-10-28 23:10:38 +0800

    Add person jsx

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Person from './Person/Person';
 import './App.css';
 
 class App extends Component {
@@ -6,8 +7,10 @@ class App extends Component {
     return (
       <div className="App">
         <h1>Hi my react app</h1>
+        <Person />
       </div>
     );
+    // return React.createElement('div', {className: 'App'}, React.createElement('h1',null, 'Does this work'))
   }
 }
 
