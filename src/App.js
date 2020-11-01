40e5076bc380a42b946f86e308498a17927639b5
jsee-twilio <ychian@gmail.com>
2020-11-01 16:45:42 +0800

    Styling

--- src/App.js
+++ src/App.js
@@ -31,10 +31,18 @@ class App extends Component {
   }
 
   render() {
+    const style = {
+      backgroundColor: 'white',
+      font: 'inherid',
+      border: '1px solid blue',
+      padding: '8px',
+      cursor: 'pointer'
+    };
+
     return (
       <div className="App">
         <h1>Hi my react app</h1>
-        <button onClick={() => this.switchNameHandler('Ron!!!')}>Switch Name</button>
+        <button style={style} onClick={() => this.switchNameHandler('Ron!!!')}>Switch Name</button>
         <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
         <Person name={this.state.persons[1].name} age={this.state.persons[1].age} 
         click={this.switchNameHandler.bind(this, 'James')} 
