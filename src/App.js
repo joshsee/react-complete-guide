f0b5daebb913821bc8992be2f63a78bd12b3ffb1
jsee-twilio <ychian@gmail.com>
2020-11-04 20:33:34 +0800

    outputing list

--- src/App.js
+++ src/App.js
@@ -50,11 +50,12 @@ class App extends Component {
     if (this.state.showPersons) {
       person = (
         <div>
-          <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
-          <Person name={this.state.persons[1].name} age={this.state.persons[1].age}
-          click={this.switchNameHandler.bind(this, 'James')} 
-          changed={this.nameChangeHandler}>My Hobbies
-          </Person>
+          {this.state.persons.map(person => {
+            return <Person 
+              name={person.name}
+              name={person.age}
+              />
+          })}
         </div>
       )
     }
